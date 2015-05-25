a031cb32e4f070d30d5895594ca93f2746622962
shvamverma@gmail.com <shivam@Struggler.(none)>
2015-05-25 15:19:36 +0530

    First Commit

--- lib/line.rb
+++ lib/line.rb
@@ -5,4 +5,14 @@ class Line
     @start_point = start_point
     @end_point = end_point
   end
+  	puts "Enter the first point:"
+	x1=gets
+	y1=gets
+	puts "Enter the second point:"
+	x2=gets
+	y2=gets
+	x=(x1.to_f)-(x2.to_f)
+	y=(y1.to_f)-(y2.to_f)
+	res=Math.sqrt(x*x+y*y)
+	puts res
 end
