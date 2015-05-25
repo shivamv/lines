857e2ff915344b4375f65f6b24edf1162745d45a
Sinister Light <me@sinisterlight.com>
2015-05-25 14:22:19 +0530

    [Shobhit] line constructor

--- lib/line.rb
+++ lib/line.rb
@@ -1,5 +1,8 @@
 class Line
-  # [1,1] , [2,2]
+  attr_accessor :start_point, :end_point
+
   def initialize start_point, end_point
+    @start_point = start_point
+    @end_point = end_point
   end
 end
